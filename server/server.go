cd8d92708e6984e05b1c4743c0f62a87df548d96
tbonatti <tiago.bonatti@gmail.com>
2022-02-19 11:10:06 -0300

    feat: create endpoint to return data from CNPJ

--- server/server.go
+++ server/server.go
@@ -2,16 +2,20 @@ package server
 
 import (
 	"net/http"
+	"t-bonatti/gopj/controller"
 
 	"github.com/gin-gonic/gin"
 )
 
 func New() *gin.Engine {
+	c := controller.NewCompanyController()
 
 	r := gin.Default()
 	r.GET("/status", func(c *gin.Context) {
 		c.String(http.StatusOK, "OK")
 	})
+
+	r.GET("/company/:cnpj", c.Get())
 
 	return r
 }
