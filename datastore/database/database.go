032bdc666e8890d4ac30b2455aa1e40720f38ad1
tbonatti <tiago.bonatti@gmail.com>
2022-04-18 21:04:43 -0300

    feat: create endpoint to return data from CNPJ

---
+++ datastore/database/database.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"t-bonatti/gopj/datastore/database/migrations"
+	"time"
+
+	"github.com/apex/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func StartDB(dsn string) {
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.WithError(err).Fatal("Could not connect to the Postgres Database")
+	}
+
+	db = database
+	config, _ := db.DB()
+	config.SetMaxIdleConns(10)
+	config.SetMaxOpenConns(100)
+	config.SetConnMaxLifetime(time.Hour)
+
+	migrations.RunMigrations(db)
+}
+
+func CloseConn() error {
+	config, err := db.DB()
+	if err != nil {
+		return err
+	}
+
+	err = config.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetDatabase() *gorm.DB {
+	return db
+}
