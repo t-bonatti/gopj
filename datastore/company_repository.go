9739ad6f7c3974eb3f6ee73f5eb88e998ca5f496
tbonatti <tiago.bonatti@gmail.com>
2022-04-21 10:02:41 -0300

    feat: rename package models to model

--- datastore/company_repository.go
+++ datastore/company_repository.go
@@ -2,13 +2,13 @@ package datastore
 
 import (
 	"t-bonatti/gopj/datastore/database"
-	"t-bonatti/gopj/models"
+	"t-bonatti/gopj/model"
 
 	"gorm.io/gorm"
 )
 
 type CompanyRepository interface {
-	Get(cnpj string) (c models.Company, err error)
+	Get(cnpj string) (c model.Company, err error)
 }
 
 type companyRepositoryImpl struct {
@@ -21,7 +21,7 @@ func NewCompanyRepository() CompanyRepository {
 	return companyRepositoryImpl{db: db}
 }
 
-func (r companyRepositoryImpl) Get(cnpj string) (c models.Company, err error) {
+func (r companyRepositoryImpl) Get(cnpj string) (c model.Company, err error) {
 	err = r.db.Where("cnpj = ?", cnpj).Find(&c).Error
 	return
 }
